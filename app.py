61eaf1240a9c9a6c338cedfe96b0096626f4abc0
sp1ra1 <stardustzjt@gmail.com>
2022-04-09 13:08:37 +0800

    change circle instersection radius from int to float

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # For geodetic coordinates (X, Y), X is longitude and Y is latitude
 from flask import Flask, request, abort, jsonify
-from werkzeug import Response
 from werkzeug.exceptions import HTTPException
 import psycopg2
 from skyfield.api import load, wgs84
@@ -10,14 +9,13 @@ app = Flask(__name__)
 
 conn = psycopg2.connect(host='206.12.92.18', dbname='propdb', user='propval', password='BCParks')
 cur = conn.cursor()
-vancouver_table_name = 'lidar_north_va'
+
+lidar_table_name = 'lidar'
 tiles_3d_table_name = 'tiles_3d'
 
 stations_url = 'https://www.celestrak.com/NORAD/elements/supplemental/gps.txt'
 satellites = load.tle_file(stations_url)
 print('Loaded', len(satellites), 'satellites')
-# by_name = {sat.name: sat for sat in satellites}
-# by_id = {sat.model.satnum: sat for sat in satellites}
 ts = load.timescale()
 
 
@@ -49,12 +47,11 @@ def lidar_polygon_intersect():
             abort(400)
 
         try:
-            # print(f"SELECT * FROM {vancouver_table_name} WHERE ST_Intersects(geo_polygon, 'POLYGON({polygon})');")
-            cur.execute(f"SELECT * FROM {vancouver_table_name} WHERE ST_Intersects(geo_polygon, 'POLYGON({polygon})');")
+            cur.execute(f"SELECT * FROM {lidar_table_name} WHERE ST_Intersects(geo_polygon, 'POLYGON({polygon})');")
             render_result = []
             result = cur.fetchall()
             for row in result:
-                render_result.append('http://206.12.92.18:10083/lidar/vancouver/' + row[0] + '.las')
+                render_result.append(f'http://206.12.92.18:10083/lidar/{row[1]}/{row[0]}.laz')
 
             return {'total_num': len(render_result), 'regions': render_result}
 
@@ -68,17 +65,17 @@ def lidar_circle_intersect():
     args = request.args
     longitude = args.get('longitude', type=float)
     latitude = args.get('latitude', type=float)
-    radius = args.get('radius', type=int)
+    radius = args.get('radius', type=float)
     if longitude is None or latitude is None or radius is None:
         abort(422)
 
     try:
         cur.execute(
-            f"SELECT * from {vancouver_table_name} where ST_Intersects(geo_polygon, ST_Buffer(ST_MakePoint({longitude}, {latitude})::geography, {radius}));")
+            f"SELECT * from {lidar_table_name} where ST_Intersects(geo_polygon, ST_Buffer(ST_MakePoint({longitude}, {latitude})::geography, {radius}));")
         render_result = []
         result = cur.fetchall()
         for row in result:
-            render_result.append('http://206.12.92.18:10083/lidar/vancouver/' + row[0] + '.las')
+            render_result.append(f'http://206.12.92.18:10083/lidar/{row[1]}/{row[0]}.laz')
         return {'total_num': len(render_result), 'regions': render_result}
 
     except Exception as e:
@@ -106,12 +103,11 @@ def tile_3d_polygon_intersect():
             abort(400)
 
         try:
-            # print(f"SELECT * FROM {vancouver_table_name} WHERE ST_Intersects(geo_polygon, 'POLYGON({polygon})');")
             cur.execute(f"SELECT * FROM {tiles_3d_table_name} WHERE ST_Intersects(geo_polygon, 'POLYGON({polygon})');")
             render_result = []
             result = cur.fetchall()
             for row in result:
-                render_result.append('http://206.12.92.18:10083/3DTiles/' + row[0] + '/')
+                render_result.append(f'http://206.12.92.18:10083/3DTiles/{row[0]}/tileset.json')
 
             return {'total_num': len(render_result), 'regions': render_result}
 
@@ -125,7 +121,7 @@ def tile_3d_circle_intersect():
     args = request.args
     longitude = args.get('longitude', type=float)
     latitude = args.get('latitude', type=float)
-    radius = args.get('radius', type=int)
+    radius = args.get('radius', type=float)
     if longitude is None or latitude is None or radius is None:
         abort(422)
 
@@ -135,7 +131,7 @@ def tile_3d_circle_intersect():
         render_result = []
         result = cur.fetchall()
         for row in result:
-            render_result.append('http://206.12.92.18:10083/3DTiles/' + row[0] + '/')
+            render_result.append(f'http://206.12.92.18:10083/3DTiles/{row[0]}/tileset.json')
         return {'total_num': len(render_result), 'regions': render_result}
 
     except Exception as e:
@@ -151,17 +147,17 @@ def get_all_satellites():
     if time is None:
         time = ts.now()
     else:
-        # UTC time with this format: 2022-03-27T13:07:22Z
+        # UTC time with this format: 20220327130722
         try:
             year = int(time[0:4])
-            month = int(time[5:7])
-            day = int(time[8:10])
-            hour = int(time[11:13])
-            minute = int(time[14:16])
-            second = int(time[17:19])
+            month = int(time[4:6])
+            day = int(time[6:8])
+            hour = int(time[8:10])
+            minute = int(time[10:12])
+            second = int(time[12:])
             time = ts.utc(year, month, day, hour, minute, second)
         except Exception as e:
-            abort(400)
+            abort(422)
 
     satellite_names = [sat.name for sat in satellites]
     satellite_ids = [str(sat.model.satnum) for sat in satellites]
@@ -170,11 +166,10 @@ def get_all_satellites():
         names = utils.split_check(names, satellite_names)
         ids = utils.split_check(ids, satellite_ids)
     except:
-        # abort(422)
-        abort(Response('invalid name or id'))
+        abort(422)
 
     if len(names) != 0 and len(ids) != 0:
-        abort(Response('names and ids are not supported at the same time'))
+        abort(422)
 
     # if no name and id specified, return the whole satellites locations
     if len(names) == 0 and len(ids) == 0:
@@ -182,7 +177,8 @@ def get_all_satellites():
         for gps in satellites:
             geocentric = gps.at(time)
             sub_point = wgs84.subpoint(geocentric)
-            response[gps.name] = {
+            response[str(gps.model.satnum)] = {
+                'name': gps.name,
                 'latitude': sub_point.latitude.degrees,
                 'longitude': sub_point.longitude.degrees,
                 'height': sub_point.elevation.m
@@ -194,7 +190,8 @@ def get_all_satellites():
         for gps in satellites:
             if gps.name in names:
                 sub_point = wgs84.subpoint(gps.at(time))
-                response[gps.name] = {
+                response[str(gps.model.satnum)] = {
+                    'name': gps.name,
                     'latitude': sub_point.latitude.degrees,
                     'longitude': sub_point.longitude.degrees,
                     'height': sub_point.elevation.m
@@ -207,32 +204,9 @@ def get_all_satellites():
             if str(gps.model.satnum) in ids:
                 sub_point = wgs84.subpoint(gps.at(time))
                 response[str(gps.model.satnum)] = {
+                    'name': gps.name,
                     'latitude': sub_point.latitude.degrees,
                     'longitude': sub_point.longitude.degrees,
                     'height': sub_point.elevation.m
                 }
         return response
-
-    # # if no name passed as parameter or the name is not in the name list, return the whole satellites locations
-    # if name is None or name not in satellite_names:
-    #     response = {}
-    #     for gps in satellites:
-    #         geocentric = gps.at(time)
-    #         sub_point = wgs84.subpoint(geocentric)
-    #         response[gps.name] = {
-    #             'latitude': sub_point.latitude.degrees,
-    #             'longitude': sub_point.longitude.degrees,
-    #             'height': sub_point.elevation.m
-    #         }
-    #     return response
-    # # if name is in the list, return the corresponding location
-    # else:
-    #     for gps in satellites:
-    #         if gps.name == name:
-    #             sub_point = wgs84.subpoint(gps.at(time))
-    #             return {name: {
-    #                 'latitude': sub_point.latitude.degrees,
-    #                 'longitude': sub_point.longitude.degrees,
-    #                 'height': sub_point.elevation.m
-    #             }}
-    #     abort(500)
